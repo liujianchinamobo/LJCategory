5d482003e7e2589ee4e95f59ae1247f748d26886
刘建LJ <liujian@chinamobo.com>
2016-09-09 16:43:29 +0800

    添加了设置圆角图片的方法

--- LJCategory/UIImage+Resize.h
+++ LJCategory/UIImage+Resize.h
@@ -11,5 +11,10 @@
 /**压缩图片的方法*/
 
 @interface UIImage (Resize)
+
+/**压缩图片都指定大小*/
 -(UIImage*)scaleImageToSize:(CGSize)newSize;
+
+/**设置圆角图片  比利用cornerRadius要高效且不卡顿*/
+- (UIImage *)cutCircleImage;
 @end
--- LJCategory/UIImage+Resize.m
+++ LJCategory/UIImage+Resize.m
@@ -27,4 +27,22 @@
     // Return the new image.
     return newImage;
 }
+
+/** 设置圆角图片 */
+- (UIImage *)cutCircleImage {
+    UIGraphicsBeginImageContextWithOptions(self.size, NO, 0.0);
+    // 获取上下文
+    CGContextRef ctr = UIGraphicsGetCurrentContext();
+    // 设置圆形
+    CGRect rect = CGRectMake(0, 0, self.size.width, self.size.height);
+    CGContextAddEllipseInRect(ctr, rect);
+    // 裁剪
+    CGContextClip(ctr);
+    // 将图片画上去
+    [self drawInRect:rect];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
 @end
