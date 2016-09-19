4220192680057ca7f8050bfc4f3201fc03d09742
刘建LJ <liujian@chinamobo.com>
2016-09-19 15:04:28 +0800

    补充点注释

--- LJCategory/UIImage+Resize.m
+++ LJCategory/UIImage+Resize.m
@@ -30,18 +30,23 @@
 
 /** 设置圆角图片 */
 - (UIImage *)cutCircleImage {
+    // 开始图像上下文
     UIGraphicsBeginImageContextWithOptions(self.size, NO, 0.0);
     // 获取上下文
     CGContextRef ctr = UIGraphicsGetCurrentContext();
-    // 设置圆形
+    // 设置范围
     CGRect rect = CGRectMake(0, 0, self.size.width, self.size.height);
+    // 画圆
     CGContextAddEllipseInRect(ctr, rect);
     // 裁剪
     CGContextClip(ctr);
     // 将图片画上去
     [self drawInRect:rect];
+    // 从上下文上获取剪裁后的照片
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    // 关闭上下文
     UIGraphicsEndImageContext();
+    
     return image;
 }
 
